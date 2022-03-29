753c1a77d41f806505debd352520d02078827b09
magantus@googlemail.com <Extrem93!>
2022-03-30 00:51:01 +0200

    update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
-app.use(express.static(__dirname + "/dist/covid19"));
+app.use(express.static(__dirname + "/www"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/covid19/index.html"));
+  res.sendFile(path.join(__dirname, "www", "index.html"));
 });
 
 app.listen(process.env.PORT || 8100);
