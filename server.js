7fe8b6e15212201ab1d49efdfd685e89b8268d59
magantus@googlemail.com <Extrem93!>
2022-03-30 00:43:37 +0200

    update

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/frontend"));
+app.use(express.static(__dirname + "/dist/covid19"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/frontend/index.html"));
+  res.sendFile(path.join(__dirname, "/dist/covid19/index.html"));
 });
 
 app.listen(process.env.PORT || 8100);
